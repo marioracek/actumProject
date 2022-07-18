595a4ea67079c8e0f01e7c0e5c091a0056d08f57
marioracek <mario.racek@smartea.tech>
2022-07-18 23:06:16 +0200

    basic project structure created

---
+++ src/test/java/tests/SearchTests.java
@@ -0,0 +1,18 @@
+package tests;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeTest;
+
+import java.time.Duration;
+
+public class SearchTests {
+
+    @BeforeTest
+    public void setup() {
+        System.setProperty("webriver.chrome.driver", "/Users/marioracek/Documents/chromedriver");
+        WebDriver driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofMillis(10000));
+        driver.get("http://automationpractice.com/index.php");
+    }
+}
