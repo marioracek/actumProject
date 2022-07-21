808566f6bc83a97e4860ebd5386c5b99de910b24
marioracek <mario.racek@smartea.tech>
2022-07-21 23:24:11 +0200

    RegistrationPO and RegistrationTests implementation

--- src/main/java/pages/RegistrationPO.java
+++ src/main/java/pages/RegistrationPO.java
@@ -2,40 +2,170 @@ package pages;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.Select;
 
 public class RegistrationPO {
 
     WebDriver driver;
 
-    By heading = By.className("page-heading");
-    By breadcrumbs = By.className("navigation_page");
-    By subHeadingRegistration = By.className("page-subheading");
-    By enterEmailText = By.cssSelector(".form_content.clearfix");
-    By emailText = By.cssSelector(".form-group.form-error");
-    By emailInput = By.id("email_create");
-    By alertError = By.cssSelector(".alert.alert-danger");
-    By createButton = By.id("SubmitCreate");
+    By signInElement = By.className("login");
+    By headingElement = By.className("page-heading");
+    By breadcrumbsElement = By.className("navigation_page");
+    By subHeadingRegistrationElement = By.className("page-subheading");
+    By enterEmailTextElement = By.cssSelector(".form_content.clearfix");
+    By emailTextElement = By.cssSelector(".form-group.form-error");
+    By emailInputElement = By.id("email_create");
+    By alertErrorElement = By.cssSelector(".alert.alert-danger");
+    By createButtonElement = By.id("SubmitCreate");
 
 
-    By gender1 = By.id("id_gender1");
-    By gender2 = By.id("id_gender2");
-    By firstNameInput = By.id("customer_firstname");
-    By lastNameInput = By.id("customer_lastname");
-    By email = By.id("email");
-    By password = By.id("passwd");
-    By newsletter = By.id("newsletter");
-    By offers = By.id("optin");
-    By firstNameAddress = By.id("firstname");
-    By lastNameAddress = By.id("lastname");
-    By address1 = By.id("address1");
-    By address2 = By.id("address2");
-    By city = By.id("city");
-    By postcode = By.id("postcode");
-    By phone = By.id("phone");
-    By phoneMobile = By.id("phone_mobile");
-    By alias = By.id("alias");
+    By gender1Element = By.id("id_gender1");
+    By gender2Element = By.id("id_gender2");
+    By firstNameInputElement = By.id("customer_firstname");
+    By lastNameInputElement = By.id("customer_lastname");
+    By yearElement = By.id("years");
+    By monthElement = By.id("months");
+    By dayElement = By.id("days");
+    By passwordElement = By.id("passwd");
+    By newsletterElement = By.id("newsletter");
+    By offersElement = By.id("optin");
+    By firstNameAddressElement = By.id("firstname");
+    By lastNameAddressElement = By.id("lastname");
+    By address1Element = By.id("address1");
+    By address2Element = By.id("address2");
+    By cityElement = By.id("city");
+    By countrySelectElement = By.id("id_country");
+    By stateSelectElement = By.id("id_state");
+    By additionalInfoElement = By.id("other");
+    By postcodeElement = By.id("postcode");
+    By phoneElement = By.id("phone");
+    By phoneMobileElement = By.id("phone_mobile");
+    By aliasReferenceElement = By.id("alias");
+    By registerButtonElement = By.id("submitAccount");
 
     public RegistrationPO(WebDriver driver) {
         this.driver = driver;
+    }
+
+    public void clickOnSignIn() {
+        driver.findElement(signInElement).click();
+    }
+
+    public String getBreadcrumbsText() {
+        return driver.findElement(breadcrumbsElement).getText();
+    }
+
+    public String getHeadingText() {
+        return driver.findElement(headingElement).getText();
+    }
+
+    public String getSubHeadingText() {
+        return driver.findElement(subHeadingRegistrationElement).getText();
+    }
+
+    public void enterEmailCreateAccount(String email) {
+        driver.findElement(emailInputElement).sendKeys(email);
+    }
+
+    public void clickCreateAnAccount() {
+        driver.findElement(createButtonElement).click();
+    }
+
+    public void clickRegisterButton() {
+        driver.findElement(registerButtonElement).click();
+    }
+
+    public void enterFirstName(String firstName) {
+        driver.findElement(firstNameInputElement).sendKeys(firstName);
+    }
+
+    public void enterLastName(String lastName) {
+        driver.findElement(lastNameInputElement).sendKeys(lastName);
+    }
+
+    public void enterPassword(String pwd) {
+        driver.findElement(passwordElement).sendKeys(pwd);
+    }
+
+    public void enterDateOfBirth(String yearToSelect, String monthToSelect, String dayToSelect) {
+        Select selectYear = new Select(driver.findElement(yearElement));
+        selectYear.selectByValue(yearToSelect);
+
+        Select selectMonth = new Select(driver.findElement(monthElement));
+        selectMonth.selectByValue(monthToSelect);
+
+        Select selectDay = new Select(driver.findElement(dayElement));
+        selectDay.selectByValue(dayToSelect);
+    }
+
+    public void checkNewsletter() {
+        driver.findElement(newsletterElement).click();
+    }
+
+    public void checkSpecialOffer() {
+        driver.findElement(offersElement).click();
+    }
+
+    public void enterFirstNameAddress(String firstName) {
+        driver.findElement(firstNameAddressElement).sendKeys(firstName);
+    }
+
+    public void enterLastNameAddress(String lastName) {
+        driver.findElement(lastNameAddressElement).sendKeys(lastName);
+    }
+
+    public void enterAddressLine1(String address) {
+        driver.findElement(address1Element).sendKeys(address);
+    }
+
+    public void enterAddressLine2(String address) {
+        driver.findElement(address2Element).sendKeys(address);
+    }
+
+    public void enterCity(String cityToEnter) {
+        driver.findElement(cityElement).sendKeys(cityToEnter);
+    }
+
+    public void selectCountry(String value) {
+        Select countryToSelect = new Select(driver.findElement(countrySelectElement));
+        countryToSelect.selectByValue(value);
+    }
+
+    public void selectState(String value) {
+        Select countryToSelect = new Select(driver.findElement(stateSelectElement));
+        countryToSelect.selectByValue(value);
+    }
+
+    public void enterAdditionalInfo(String info) {
+        driver.findElement(additionalInfoElement).sendKeys(info);
+    }
+
+    public void enterPhone(String phone) {
+        driver.findElement(phoneElement).sendKeys(phone);
+    }
+
+    public void enterMobilePhone(String mobilePhone) {
+        driver.findElement(phoneMobileElement).sendKeys(mobilePhone);
+    }
+
+    public void enterPostalCode(String postalCode) {
+        driver.findElement(postcodeElement).sendKeys(postalCode);
+    }
+
+    public void enterGender(String gender) {
+        switch (gender) {
+            case "Mr.":
+                driver.findElement(gender1Element).click();
+                break;
+            case "Mrs.":
+                driver.findElement(gender2Element).click();
+                break;
+            default:
+                break;
+        }
+    }
+
+    public void enterAliasReference(String reference) {
+        driver.findElement(aliasReferenceElement).sendKeys(reference);
     }
 }
--- src/test/java/tests/RegistrationTests.java
+++ src/test/java/tests/RegistrationTests.java
@@ -1,18 +1,74 @@
 package tests;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+import pages.RegistrationPO;
 
 import java.time.Duration;
 
+import static org.testng.Assert.assertEquals;
+
 public class RegistrationTests {
+
+    WebDriver driver;
+    RegistrationPO registrationPO;
 
     @BeforeTest
     public void setup() {
         System.setProperty("webriver.chrome.driver", "/Users/marioracek/Documents/chromedriver");
-        WebDriver driver = new ChromeDriver();
+        driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(Duration.ofMillis(10000));
+        driver.manage().window().maximize();
         driver.get("http://automationpractice.com/index.php");
+    }
+
+    @Test
+    public void checkCorrectInputs() {
+        registrationPO = new RegistrationPO(driver);
+        registrationPO.clickOnSignIn();
+        new WebDriverWait(driver, Duration.ofSeconds(30)).until(ExpectedConditions.presenceOfElementLocated(By.id("create-account_form")));
+        checkCorrectPage("AUTHENTICATION", "Authentication");
+        assertEquals(registrationPO.getSubHeadingText(), "CREATE AN ACCOUNT", "Create an account text is correct");
+        registrationPO.enterEmailCreateAccount("mario.racek@seznam.fr");
+        registrationPO.clickCreateAnAccount();
+        new WebDriverWait(driver, Duration.ofSeconds(30)).until(ExpectedConditions.presenceOfElementLocated(By.id("account-creation_form")));
+        checkCorrectPage("CREATE AN ACCOUNT", "Authentication");
+        assertEquals(registrationPO.getSubHeadingText(), "YOUR PERSONAL INFORMATION", "Create an account text is correct");
+        registrationPO.enterGender("Mr.");
+        registrationPO.enterFirstName("Mario");
+        registrationPO.enterLastName("Racek");
+        registrationPO.enterPassword("password");
+        registrationPO.enterDateOfBirth("1991", "11", "11");
+        registrationPO.checkNewsletter();
+        registrationPO.checkSpecialOffer();
+        registrationPO.enterFirstNameAddress("Mario");
+        registrationPO.enterLastNameAddress("Racek");
+        registrationPO.enterAddressLine1("Sturova 11");
+        registrationPO.enterCity("Singapur");
+        registrationPO.selectCountry("21");
+        registrationPO.selectState("8");
+        registrationPO.enterPostalCode("14900");
+        registrationPO.enterAdditionalInfo("Info");
+        registrationPO.enterMobilePhone("123456789");
+        registrationPO.enterAliasReference("moja");
+        registrationPO.clickRegisterButton();
+        checkCorrectPage("MY ACCOUNT", "My account");
+    }
+
+    public void checkCorrectPage(String heading, String breadcrumb) {
+        registrationPO = new RegistrationPO(driver);
+        assertEquals(registrationPO.getBreadcrumbsText(), breadcrumb, "Breadcrumbs text is correct");
+        assertEquals(registrationPO.getHeadingText(), heading, "Title text is correct");
+    }
+
+    @AfterTest
+    public void close() {
+        driver.close();
     }
 }
